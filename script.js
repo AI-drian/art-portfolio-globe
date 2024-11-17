5b106127d192caf459c0148b5e675c2f577c051b
Adrian Andreasson <adrian.andreasson@outlook.com>
2024-11-17 13:27:04 +0100

    Initial commit for my art portfolio globe

---
+++ script.js
@@ -0,0 +1,15 @@
+// Initialize the globe
+const globe = Globe()(document.getElementById('globe-container'))
+  .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg') // Add a dark earth texture
+  .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png') // Add 3D texture
+  .backgroundColor('#000000') // Black background
+  .showAtmosphere(true) // Enable atmosphere
+  .atmosphereColor('#87ceeb') // Light blue atmosphere
+  .atmosphereAltitude(0.25); // Set atmosphere height
+
+// Add automatic rotation
+globe.controls().autoRotate = true;
+globe.controls().autoRotateSpeed = 0.5;
+
+// Console log when ready
+console.log('The globe is up and spinning!');
